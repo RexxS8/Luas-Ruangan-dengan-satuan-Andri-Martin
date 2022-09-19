5a0a62ed4e95fa6044ade2cdebdcbefd9527ab7a
RexxS8 <andri4martin@gmail.com>
2022-09-19 20:21:53 +0700

    Create Luas Ruangan with satuan.py

---
+++ Luas Ruangan with satuan.py
@@ -0,0 +1,7 @@
+#Andri Martin - 064002200010
+print("Hitung Luas Ruangan")
+a = float(input("Masukan Panjang Ruangan:"))
+b = float(input("Masukan Lebar Ruangan:"))
+satuan = input("Masukan Satuan(Meter/Inci):")
+luas = a*b
+print("luas Ruangan dengan panjang:", a , "dan lebar", b , "adalah", (luas), satuan)
